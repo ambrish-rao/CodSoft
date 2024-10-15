210bcadb31e979f855ea126d56fc9af3999f7eb0
Ambrish Rao <raoambrish956@gmail.com>
2024-10-16 00:48:45 +0530

    Update passwordGenerator.py

--- codSoft/passwordGenerator.py
+++ codSoft/passwordGenerator.py
@@ -8,8 +8,17 @@ str_1 = string.ascii_lowercase # Contains all lower case letters
 str_2 = string.ascii_uppercase # Contains all Upper case letters
 dig = string.digits # Contains all Numbers
 punc = string.punctuation # Contains all the puntuations
-pass_len = int(input("Enter password length : ")) # Enter Password length by User...
 
+# Function to get valid Password length..
+def password_len():
+    pass_len = input("Enter password length(only Integer Value) : ") # Enter Password length by User...
+    # we have to keep asking until valid integer is entered.
+    while not pass_len.isdigit(): #check whether the input is integer or not
+        print("Invalid Input! Please Enter Integer Value")
+        pass_len = input("Enter password length(only Integer Value) : ")
+    return int(pass_len)
+pass_len = password_len() #get Password length
+    
 lst = [] 
     #Empty list to extend all letters numbers punctuations. extend will extract only elements inside the list not whole list..
 lst.extend(list(str_1))
@@ -20,5 +29,3 @@ lst.extend(list(punc))
 # print Your Strong Password.
 print("Ypur password is: ")
 print("".join(random.sample(lst, pass_len)))
-    
-
